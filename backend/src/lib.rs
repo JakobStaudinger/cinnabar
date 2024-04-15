361da64aa3b4c84c04098f20ca76cfccabd9aa73
Jakob Staudinger <jakobstaudi@gmail.com>
2024-04-15 22:37:15 +0200

    get config file from github repo

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,15 +1,73 @@
+use base64::{engine::general_purpose::STANDARD, Engine as _};
+
 use domain::{Pipeline, PipelineId};
+use jsonwebtoken::EncodingKey;
+use octocrab::{
+    models::{AppId, InstallationToken},
+    params::apps::CreateInstallationAccessToken,
+    Octocrab,
+};
+use url::Url;
 
 mod domain;
 mod runner;
 
 pub async fn main() {
-    let configuration = serde_json::from_str(
-        std::fs::read_to_string("assets/test-pipeline.json")
+    let github_app_id = std::env::var("GITHUB_APP_ID").unwrap().parse().unwrap();
+    let github_private_key = std::env::var("GITHUB_PRIVATE_KEY").unwrap();
+
+    let octocrab = Octocrab::builder()
+        .app(
+            AppId(github_app_id),
+            EncodingKey::from_rsa_pem(github_private_key.as_bytes()).unwrap(),
+        )
+        .build()
+        .unwrap();
+
+    let installation = octocrab
+        .apps()
+        .get_repository_installation("JakobStaudinger", "rust-ci")
+        .await
+        .unwrap();
+
+    let mut create_access_token = CreateInstallationAccessToken::default();
+    create_access_token.repositories = vec!["rust-ci".to_owned()];
+
+    let access_token_url = Url::parse(installation.access_tokens_url.as_ref().unwrap()).unwrap();
+    let access: InstallationToken = octocrab
+        .post(access_token_url.path(), Some(&create_access_token))
+        .await
+        .unwrap();
+
+    let octocrab = Octocrab::builder()
+        .personal_token(access.token)
+        .build()
+        .unwrap();
+
+    let content = octocrab
+        .repos("JakobStaudinger", "rust-ci")
+        .get_content()
+        .path("assets/test-pipeline.json")
+        .send()
+        .await
+        .unwrap();
+
+    let configuration = String::from_utf8_lossy(
+        STANDARD
+            .decode(
+                content.items[0]
+                    .content
+                    .as_ref()
+                    .unwrap()
+                    .split('\n')
+                    .collect::<String>(),
+            )
             .unwrap()
-            .as_str(),
+            .as_ref(),
     )
-    .unwrap();
+    .to_string();
+
+    let configuration = serde_json::from_str(&configuration).unwrap();
 
     let pipeline = Pipeline::new(PipelineId::new(1), configuration);
 
