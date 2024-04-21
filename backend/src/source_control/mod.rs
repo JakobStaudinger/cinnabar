ad576c7c8b24cbf1ff8e1683bfde7cc65d1cfe25
Jakob Staudinger <jakobstaudi@gmail.com>
2024-04-21 12:27:03 +0200

    implement status checks

--- backend/src/source_control/mod.rs
+++ backend/src/source_control/mod.rs
@@ -8,4 +8,15 @@ pub trait SourceControl {
 
 pub trait SourceControlInstallation {
     async fn read_file_contents(&self, path: &str) -> Result<String, impl std::error::Error>;
+    async fn update_status_check(
+        &self,
+        commit: &str,
+        status: CheckStatus,
+    ) -> Result<(), impl std::error::Error>;
+}
+
+pub enum CheckStatus {
+    Pending,
+    Failed,
+    Passed,
 }
