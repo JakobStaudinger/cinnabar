31f068861d6c01584a387bbfde0b1aab15943f2f
Jakob Staudinger <jakobstaudi@gmail.com>
2024-05-18 13:17:22 +0200

    verify webhook payload integrity

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -6,6 +6,9 @@ use axum::{
 };
 use bollard::Docker;
 use domain::{Pipeline, PipelineId, PipelineStatus};
+use hmac::{Hmac, Mac};
+use secrecy::{ExposeSecret, SecretString};
+use sha2::Sha256;
 use source_control::{github::GitHub, CheckStatus, SourceControl, SourceControlInstallation};
 use std::io;
 use tokio::signal::{self, unix::SignalKind};
@@ -54,7 +57,13 @@ async fn shutdown_signal() {
     }
 }
 
-async fn handle_webhook(headers: HeaderMap) -> impl IntoResponse {
+async fn handle_webhook(headers: HeaderMap, body: String) -> impl IntoResponse {
+    let webhook_secret = SecretString::new(std::env::var("GITHUB_WEBHOOK_SECRET").unwrap());
+
+    if let Err(message) = verify_checksum(&headers, &body, &webhook_secret) {
+        return (StatusCode::BAD_REQUEST, message);
+    }
+
     if let Some(event) = headers.get("x-github-event") {
         match event.to_str() {
             Ok("push") => {
@@ -113,10 +122,81 @@ async fn handle_webhook(headers: HeaderMap) -> impl IntoResponse {
     }
 }
 
+fn verify_checksum(
+    headers: &HeaderMap,
+    body: &String,
+    secret: &SecretString,
+) -> Result<(), &'static str> {
+    let expected_signature = headers
+        .get("x-hub-signature-256")
+        .ok_or("Missing header x-hub-signature-256")?
+        .to_str()
+        .map_err(|_| "Failed to parse x-hub-signature-256 header")?;
+
+    let expected_signature = expected_signature
+        .strip_prefix("sha256=")
+        .ok_or("Malformed sha256 header")?;
+
+    let expected_signature =
+        hex::decode(expected_signature).map_err(|_| "Failed to parse sha256 signature")?;
+
+    let mut mac = Hmac::<Sha256>::new_from_slice(secret.expose_secret().as_bytes())
+        .map_err(|_| "Failed to hash payload")?;
+
+    mac.update(body.as_bytes());
+
+    mac.verify_slice(expected_signature.as_slice())
+        .map_err(|_| "Failed to verify sha256 checksum")
+}
+
 #[cfg(test)]
 mod tests {
+    use axum::http::{HeaderMap, HeaderValue};
+    use secrecy::SecretString;
+
+    use crate::verify_checksum;
+
     #[test]
-    fn example_test() {
-        assert_eq!(1 + 1, 2);
+    fn verify_checksum_should_return_ok() {
+        let secret = SecretString::new("It's a Secret to Everybody".to_string());
+        let body = "Hello, World!".to_string();
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "X-Hub-Signature-256",
+            HeaderValue::from_static(
+                "sha256=757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17",
+            ),
+        );
+
+        assert_eq!(verify_checksum(&headers, &body, &secret), Ok(()));
+    }
+
+    #[test]
+    fn verify_checksum_should_return_err_if_header_is_missing() {
+        let secret = SecretString::new("It's a Secret to Everybody".to_string());
+        let body = "Hello, World!".to_string();
+        let headers = HeaderMap::new();
+
+        assert_eq!(
+            verify_checksum(&headers, &body, &secret),
+            Err("Missing header x-hub-signature-256")
+        );
+    }
+
+    #[test]
+    fn verify_checksum_should_return_err_if_checksum_differs() {
+        let secret = SecretString::new("It's a Secret to Everybody".to_string());
+        let body = "Hello, World!".to_string();
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "X-Hub-Signature-256",
+            HeaderValue::from_static(
+                "sha256=757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e16",
+            ),
+        );
+        assert_eq!(
+            verify_checksum(&headers, &body, &secret),
+            Err("Failed to verify sha256 checksum")
+        );
     }
 }
