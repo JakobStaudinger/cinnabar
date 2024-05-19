61ac1225058e0cce5b96e232f29ca01388b125a5
Jakob Staudinger <jakobstaudi@gmail.com>
2024-05-19 23:41:41 +0200

    refactor build_config

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -30,23 +30,7 @@ struct RequestState {
 }
 
 pub async fn main() -> Result<(), String> {
-    let github_webhook_secret = SecretString::new(
-        std::env::var("GITHUB_WEBHOOK_SECRET")
-            .map_err(|_| "Please provide the GITHUB_WEBHOOK_SECRET environment variable")?,
-    );
-    let github_app_id = std::env::var("GITHUB_APP_ID")
-        .map_err(|_| "Please provide the GITHUB_APP_ID environment variable")?
-        .parse()
-        .map_err(|_| "GITHUB_APP_ID needs to be an integer")?;
-    let github_private_key = SecretString::new(
-        std::env::var("GITHUB_PRIVATE_KEY")
-            .map_err(|_| "Please provide the GITHUB_PRIVATE_KEY environment variable")?,
-    );
-    let config = AppConfig {
-        github_webhook_secret,
-        github_app_id,
-        github_private_key,
-    };
+    let config = build_config()?;
 
     start_http_server(config)
         .await
@@ -72,6 +56,27 @@ async fn start_http_server(config: AppConfig) -> Result<(), io::Error> {
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown_signal())
         .await
+}
+
+fn build_config() -> Result<AppConfig, String> {
+    let github_webhook_secret = SecretString::new(
+        std::env::var("GITHUB_WEBHOOK_SECRET")
+            .map_err(|_| "Please provide the GITHUB_WEBHOOK_SECRET environment variable")?,
+    );
+    let github_app_id = std::env::var("GITHUB_APP_ID")
+        .map_err(|_| "Please provide the GITHUB_APP_ID environment variable")?
+        .parse()
+        .map_err(|_| "GITHUB_APP_ID needs to be an integer")?;
+    let github_private_key = SecretString::new(
+        std::env::var("GITHUB_PRIVATE_KEY")
+            .map_err(|_| "Please provide the GITHUB_PRIVATE_KEY environment variable")?,
+    );
+
+    Ok(AppConfig {
+        github_webhook_secret,
+        github_app_id,
+        github_private_key,
+    })
 }
 
 async fn shutdown_signal() {
