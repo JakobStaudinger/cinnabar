a4751677e83f835032d0abe9fe5f701233201b9c
Jakob Staudinger <jakobstaudi@gmail.com>
2024-09-17 13:30:34 +0200

    repository pattern

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -18,6 +18,8 @@ async fn main() -> Result<(), String> {
     let connection = &mut SqliteConnection::establish(&config.database.url)
         .map_err(|e| format!("Failed to connect to database {e}"))?;
 
+    let mut repository = PostsRepositoryImpl { connection };
+
     let args = std::env::args().skip(1).collect::<Vec<_>>();
     let args = args.iter().map(|arg| arg.as_str()).collect::<Vec<_>>();
 
@@ -38,8 +40,13 @@ async fn main() -> Result<(), String> {
                 .read_to_string(&mut body)
                 .map_err(|e| format!("Failed to read from stdin: {e}"))?;
 
-            let post = create_post(connection, &title, &body)?;
-            println!("Saved draft post {title} with id {}", post.id);
+            let body = body.as_str();
+
+            let post_id = repository
+                .create_post(&NewPost { title, body })
+                .map_err(|_| "Failed to insert post")?;
+
+            println!("Saved draft post {title} with id {post_id}");
         }
         ["write", ..] => println!("Syntax: cargo run write"),
         ["publish", post_id] => {
@@ -49,15 +56,11 @@ async fn main() -> Result<(), String> {
 
             println!("Publishing post {post_id}");
 
-            use self::schema::posts::dsl::{posts, published};
+            repository
+                .publish_post(post_id)
+                .map_err(|_| "Failed to publish post")?;
 
-            let post = diesel::update(posts.find(post_id))
-                .set(published.eq(true))
-                .returning(Post::as_returning())
-                .get_result(connection)
-                .map_err(|e| format!("Failed to publish post {post_id}: {e}"))?;
-
-            println!("Published post {}", post.title);
+            println!("Published post {post_id}");
         }
         ["publish", ..] => println!("Syntax: cargo run publish <id>"),
         ["unpublish", post_id] => {
@@ -67,15 +70,11 @@ async fn main() -> Result<(), String> {
 
             println!("Unpublishing post {post_id}");
 
-            use self::schema::posts::dsl::{posts, published};
+            repository
+                .unpublish_post(post_id)
+                .map_err(|_| "Failed to unpublish post")?;
 
-            let post = diesel::update(posts.find(post_id))
-                .set(published.eq(false))
-                .returning(Post::as_returning())
-                .get_result(connection)
-                .map_err(|e| format!("Faield to unpublish post {post_id}: {e}"))?;
-
-            println!("Unpublished post {}", post.title);
+            println!("Unpublished post {post_id}");
         }
         ["unpublish", ..] => println!("Syntax: cargo run unpublish <id>"),
         ["delete", post_id] => {
@@ -85,24 +84,19 @@ async fn main() -> Result<(), String> {
 
             println!("Deleting post {post_id}");
 
-            use self::schema::posts::dsl::*;
+            repository
+                .delete_post(post_id)
+                .map_err(|_| "Failed to delete post")?;
 
-            diesel::delete(posts.find(post_id).filter(published.eq(false)))
-                .execute(connection)
-                .map_err(|e| format!("Could not delete post {post_id}: {e}"))?;
+            println!("Deleted post {post_id}");
         }
         ["delete", ..] => println!("Syntax: cargo run delete <id>"),
         _ => {
-            use self::schema::posts::dsl::*;
+            let posts = repository
+                .get_published_posts()
+                .map_err(|_| "Failed to query posts")?;
 
-            let results = posts
-                .filter(published.eq(true))
-                .limit(5)
-                .select(Post::as_select())
-                .load(connection)
-                .map_err(|e| format!("Failed to query posts: {e}"))?;
-
-            for post in results {
+            for post in posts {
                 println!("{}", post.title);
                 println!("------------");
                 println!("{}", post.body);
@@ -120,18 +114,6 @@ async fn main() -> Result<(), String> {
     Ok(())
 }
 
-fn create_post(conn: &mut SqliteConnection, title: &str, body: &str) -> Result<Post, String> {
-    use crate::schema::posts;
-
-    let new_post = NewPost { title, body };
-
-    diesel::insert_into(posts::table)
-        .values(&new_post)
-        .returning(Post::as_returning())
-        .get_result(conn)
-        .map_err(|e| format!("Failed to insert post: {e}"))
-}
-
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::schema::posts)]
 pub struct Post {
@@ -146,4 +128,79 @@ pub struct Post {
 pub struct NewPost<'a> {
     pub title: &'a str,
     pub body: &'a str,
+}
+
+trait PostsRepository {
+    fn get_published_posts(&mut self) -> Result<Vec<Post>, ()>;
+    fn create_post(&mut self, post: &NewPost) -> Result<i32, ()>;
+    fn publish_post(&mut self, post_id: i32) -> Result<(), ()>;
+    fn unpublish_post(&mut self, post_id: i32) -> Result<(), ()>;
+    fn delete_post(&mut self, post_id: i32) -> Result<(), ()>;
+}
+
+struct PostsRepositoryImpl<'a> {
+    connection: &'a mut SqliteConnection,
+}
+
+impl<'a> PostsRepository for PostsRepositoryImpl<'a> {
+    fn get_published_posts(&mut self) -> Result<Vec<Post>, ()> {
+        use crate::schema::posts::dsl::*;
+
+        posts
+            .filter(published.eq(true))
+            .limit(5)
+            .select(Post::as_select())
+            .load(self.connection)
+            .map_err(|_| ())
+    }
+
+    fn create_post(&mut self, post: &NewPost) -> Result<i32, ()> {
+        use crate::schema::posts;
+
+        let post = diesel::insert_into(posts::table)
+            .values(post)
+            .returning(Post::as_returning())
+            .get_result(self.connection)
+            .map_err(|e| format!("Failed to insert post: {e}"))
+            .map_err(|_| ())?;
+
+        Ok(post.id)
+    }
+
+    fn publish_post(&mut self, post_id: i32) -> Result<(), ()> {
+        use self::schema::posts::dsl::{posts, published};
+
+        diesel::update(posts.find(post_id))
+            .set(published.eq(true))
+            .returning(Post::as_returning())
+            .get_result(self.connection)
+            .map_err(|_| ())?;
+
+        Ok(())
+    }
+
+    fn unpublish_post(&mut self, post_id: i32) -> Result<(), ()> {
+        use self::schema::posts::dsl::{posts, published};
+
+        diesel::update(posts.find(post_id))
+            .set(published.eq(false))
+            .returning(Post::as_returning())
+            .get_result(self.connection)
+            .map_err(|_| ())?;
+
+        Ok(())
+    }
+
+    fn delete_post(&mut self, post_id: i32) -> Result<(), ()> {
+        use self::schema::posts::dsl::*;
+
+        let result = diesel::delete(posts.find(post_id).filter(published.eq(false)))
+            .execute(self.connection)
+            .map_err(|_| ())?;
+
+        match result {
+            1 => Ok(()),
+            _ => Err(()),
+        }
+    }
 }
